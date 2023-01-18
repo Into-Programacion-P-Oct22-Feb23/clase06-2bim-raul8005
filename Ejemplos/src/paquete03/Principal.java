ff2e96781a6e7a2040db69d354c7ec229b93c5ce
raul8005 <rimedina@utpl.edu.ec>
2023-01-18 12:23:27 -0500

    Ejemplos clase

--- Ejemplos/src/paquete03/Principal.java
+++ Ejemplos/src/paquete03/Principal.java
@@ -7,6 +7,7 @@ package paquete03;
 
 import paquete04.OperadorUno;
 import paquete04.OperadorDos;
+import paquete05.OperadorTres;
 
 /**
  *
@@ -18,9 +19,14 @@ public class Principal {
         int valorA = OperadorUno.obtenerNumero(100);
         int valorB = OperadorUno.obtenerNumero(10);
         int suma = OperadorDos.obtenerSuma(valorA, valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n",
+        int multiplicacion = OperadorTres.obtenerMultiplicacion(valorA, valorB);
+        System.out.printf("La suma de %d + %d es igual a: %d\nLa multiplicacion"
+                + "de %d * %d es igual a: %d\n",
                 valorA,
                 valorB,
-                suma);
+                suma,
+                valorA,
+                valorB,
+                multiplicacion);
     }
 }
